e215d09014e269b9fd44a52a8e073eb386dbce3d
Tom Osowski <osowskit@github.com>
2018-09-20 20:37:27 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # rm .jira-cl.json
-printf  '{"protocol":"https","host":"%s","username":"%s","password":"%s","apiVersion":"2","strictSSL":true}' "$HOST" "$USERNAME" "$JIRA_API_TOKEN" >> .jira-cl.json
+printf  '{"protocol":"https","host":"%s","username":"%s","password":"%s","apiVersion":"2","strictSSL":true}' "$HOST" "$USERNAME" "$JIRA_API_TOKEN" >> ~/.jira-cl.json
